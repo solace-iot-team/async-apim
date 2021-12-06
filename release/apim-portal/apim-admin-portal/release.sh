f5785123f99f7d01be8736c5992340656e8fd8d5
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-12-06 09:36:27 +0100

    docker release for admin-portal

---
+++ release/apim-portal/apim-admin-portal/release.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+scriptDir=$(cd $(dirname "$0") && pwd);
+scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
+
+
+############################################################################################################################
+# Run
+
+echo " >>> Starting $scriptName ..."
+
+echo "implement me "; exit 1;
+
+
+# echo " >>> Install ..."
+#   cd $scriptDir
+#   runScript="npm install"
+#   $runScript
+#   code=$?;
+#   if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1; fi
+# echo " >>> Success."
+
+# echo " >>> Build+Push..."
+#   cd $scriptDir
+#   runScript="npm run build+push"
+#   $runScript
+#   code=$?;
+#   if [[ $code == 2 ]]; then
+#     echo ">>> nothing to do, version already exists - code=$code - $runScript' - $scriptName"; exit 0;
+#   elif [[ $code != 0 ]]; then
+#     echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1;
+#   fi
+# echo " >>> Success."
+
+
+###
+# The End.
