7c21a4c82e60d0844adb3f14cf36a6221ae7ee66
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-12-06 17:59:18 +0100

    initial quickstart

--- quickstart/start.sh
+++ quickstart/start.sh
@@ -7,19 +7,28 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 
   dockerProjectName="qs-async-apim"
   dockerComposeFile="$scriptDir/docker.compose.yml"
-  export QS_ASYNC_APIM_CONNECTOR_CONTAINER_NAME="qs-async-apim-connector"
-
+  # export QS_ASYNC_APIM_CONNECTOR_CONTAINER_NAME="qs-async-apim-connector"
+  # export QS_ASYNC_APIM_SERVER_MONGODB_DATA_MOUNT_PATH="$scriptDir/docker-volumes/apim-server-mongodb-data"
+  # export QS_ASYNC_APIM_CONNECTOR_MONGODB_DATA_MOUNT_PATH="$scriptDir/docker-volumes/apim-connector-mongodb-data"
+  # export QS_ASYNC_APIM_WWW_CONF_MOUNT_PATH="$scriptDir/docker-volumes/apim-www"
+  # export QS_ASYNC_APIM_WWW_LOGS_MOUNT_PATH="$scriptDir/docker-volumes/apim-www-logs"
 ############################################################################################################################
 # Run
+
+# mkdir -p $QS_ASYNC_APIM_SERVER_MONGODB_DATA_MOUNT_PATH
+# mkdir -p $QS_ASYNC_APIM_CONNECTOR_MONGODB_DATA_MOUNT_PATH
+# mkdir -p $QS_ASYNC_APIM_WWW_LOGS_MOUNT_PATH
 
 export DOCKER_CLIENT_TIMEOUT=120
 export COMPOSE_HTTP_TIMEOUT=120
 
 echo " >>> Docker-compose down for project: $dockerProjectName ..."
-  docker-compose -p $dockerProjectName -f "$dockerComposeFile" down --volumes 
-  # docker-compose -p $dockerProjectName -f "$dockerComposeFile" down --volumes --rmi all
+  # docker-compose -p $dockerProjectName -f "$dockerComposeFile" down --volumes
+  docker-compose -p $dockerProjectName -f "$dockerComposeFile" down --volumes --rmi all
   if [[ $? != 0 ]]; then echo " >>> ERROR: docker compose down for '$dockerProjectName'"; exit 1; fi
 echo " >>> Success."
+
+# echo "continue here"; exit 1
 
 echo " >>> Docker-compose up for project: $dockerProjectName ..."
 
@@ -27,38 +36,6 @@ echo " >>> Docker-compose up for project: $dockerProjectName ..."
   if [[ $? != 0 ]]; then echo " >>> ERROR: docker compose up for '$dockerProjectName'"; exit 1; fi
 
   docker ps -a
-
-  # containerName="$APIM_PORTAL_CONNECTOR_CONTAINER_NAME"
-  # echo "   >>> check: docker logs $containerName ..."
-  #   WORKING_DIR=$scriptDir/tmp; mkdir -p $WORKING_DIR; rm -rf $WORKING_DIR/*;
-  #   dockerLogsFile="$WORKING_DIR/$containerName.docker.logs"
-  #   isInitialized=0; checks=0
-  #   isServerListening=0;
-  #   isConnected2Mongo=0;
-  #   isUserRegistryLoaded=0;
-  #   until [[ $isInitialized -gt 2 || $checks -gt 10 ]]; do
-  #     ((checks++))
-  #     echo "   check: $checks"
-  #     docker logs $containerName > $dockerLogsFile
-  #     if [[ $? != 0 ]]; then echo " >>> ERROR: docker logs '$containerName'"; exit 1; fi
-  #
-  #     entryListeningOnPort=$(grep -n -e "Listening on port" $dockerLogsFile)
-  #     echo "      - entryListeningOnPort='$entryListeningOnPort'"
-  #     if [ ! -z "$entryListeningOnPort" ]; then isServerListening=1; fi
-  #
-  #     entryConnected2Mongo=$(grep -n -e "Connected to Mongo" $dockerLogsFile)
-  #     echo "      - entryConnected2Mongo='$entryConnected2Mongo'"
-  #     if [ ! -z "$entryConnected2Mongo" ]; then isConnected2Mongo=1; fi
-  #
-  #     entryLoadedUserRegistry=$(grep -n -e "Loaded user registry" $dockerLogsFile)
-  #     echo "      - entryLoadedUserRegistry='$entryLoadedUserRegistry'"
-  #     if [ ! -z "$entryLoadedUserRegistry" ]; then isUserRegistryLoaded=1; fi
-  #
-  #     isInitialized=$(($isServerListening + $isConnected2Mongo + $isUserRegistryLoaded));
-  #     if [ $isInitialized -lt 3 ]; then sleep 2s; fi
-  #   done
-  #   if [ $isInitialized -lt 3 ]; then echo " >>> ERROR: server is not initialized, checks=$checks"; exit 1; fi
-  # echo "   >>> success."
 
 echo " >>> Success."
 
