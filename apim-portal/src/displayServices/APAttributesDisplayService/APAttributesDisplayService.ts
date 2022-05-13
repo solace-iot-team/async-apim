a6a955f9ca2e11ca5760283c1fdce656709f79d2
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-05-13 10:38:01 +0200

    fixed attribute validation

--- apim-portal/src/displayServices/APAttributesDisplayService/APAttributesDisplayService.ts
+++ apim-portal/src/displayServices/APAttributesDisplayService/APAttributesDisplayService.ts
@@ -89,8 +89,13 @@ class APAttributesDisplayService {
   public create_ApRawAttribute(apAttributeDisplay: IAPAttributeDisplay): TAPRawAttribute {
     const funcName = 'create_ApRawAttribute';
     const logName = `${this.BaseComponentName}.${funcName}()`;
-    // validate value
-    if(!this.validateAttributeValue(apAttributeDisplay.value)) throw new Error(`${logName}: invalid attribute value = ${apAttributeDisplay.value}`);
+    // validate value if not empty
+    if(
+      apAttributeDisplay.apEntityId.id !== '' &&
+      !this.validateAttributeValue(apAttributeDisplay.value)
+    ) {
+      throw new Error(`${logName}: invalid attribute value, attribute=${JSON.stringify(apAttributeDisplay)}`);
+    }
     return {
       name: apAttributeDisplay.apEntityId.id,
       // value: this.construct_RawAttributeValue(apAttributeDisplay.value)
