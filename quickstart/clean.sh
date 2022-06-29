6d7ad015533c6aba427e7c88ebad6c4df03863bf
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-06-29 14:21:16 +0200

    fixed single mongo db path

--- quickstart/clean.sh
+++ quickstart/clean.sh
@@ -5,22 +5,15 @@ scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
 ############################################################################################################################
 # Settings
 
-  apimServerMongoDBDataPath="$scriptDir/docker-volumes/apim-server-mongodb-data/*"
-  apimConnectorMongoDBDataPath="$scriptDir/docker-volumes/apim-connector-mongodb-data/*"
+  apimMongoDBDataPath="$scriptDir/docker-volumes/apim-mongodb-data/*"
 
 ############################################################################################################################
 # Run
 
-echo " >>> Deleting $apimServerMongoDBDataPath ..."
-  rm -rf $apimServerMongoDBDataPath
+echo " >>> Deleting $apimMongoDBDataPath ..."
+  rm -rf $apimMongoDBDataPath
   if [[ $? != 0 ]]; then echo " >>> ERROR: deleting db files"; exit 1; fi
 echo " >>> Success."
-
-echo " >>> Deleting $apimConnectorMongoDBDataPath ..."
-  rm -rf $apimConnectorMongoDBDataPath
-  if [[ $? != 0 ]]; then echo " >>> ERROR: deleting db files"; exit 1; fi
-echo " >>> Success."
-
 
 ###
 # The End.
