fb0c4c522c63ca48d506f4a67c5b7bc627b585fa
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-08-12 09:33:12 +0200

    increment patch instead of minor

--- apim-portal/src/displayServices/APVersioningDisplayService.ts
+++ apim-portal/src/displayServices/APVersioningDisplayService.ts
@@ -286,7 +286,7 @@ class APVersioningDisplayService {
 
   public create_NextVersion(version: string): string {
     const versionSemVer = new SemVer(version);
-    versionSemVer.inc("minor");
+    versionSemVer.inc("patch");
     return versionSemVer.format();
   }
   
