f16dc4c5e3b8e07c8c2a290180a29a330784af73
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-10-19 13:13:05 +0200

    fixed zip file download url

--- apim-portal/src/utils/APFetch.ts
+++ apim-portal/src/utils/APFetch.ts
@@ -96,6 +96,12 @@ export class APFetch {
 
     const connectorOpenApiConfig: any = await APSClientOpenApi.getConnectorClientOpenApiConfig();
     console.log(`${logName}: connectorOpenApiConfig=${JSON.stringify(connectorOpenApiConfig, null, 2)}`);
+    // e.GETConnector_ZipContents(): connectorOpenApiConfig={
+    //   "BASE": "/apim-server/v1/connectorProxy/v1",
+    //   "VERSION": "0.15.2",
+    //   "WITH_CREDENTIALS": true,
+    //   "TOKEN": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJtYXN0ZXIudXNlckBhc3luYy1hcGltLWRldmVsLmNvbSIsImlhdCI6MTY2NjE3NzE2MDQ0OSwiYWNjb3VudFR5cGUiOiJVU0VSX0FDQ09VTlQiLCJleHAiOjE2NjYxNzcxNjEzNDksImlzcyI6InFzLWFzeW5jLWFwaW0tc2VydmVyIiwic3ViIjoibWFzdGVyLnVzZXJAYXN5bmMtYXBpbS1kZXZlbC5jb20ifQ.Y_nrXJLwPHnRfUtMGEOUUcvJSMzn7XyNPKbJy9xEdRY"
+    // }
     // console.warn(`${logName}: connectorOpenApiConfig = ${JSON.stringify(connectorOpenApiConfig)}`);
     // throw new Error(`${logName}: check the connectorOpenApiConfig`);
     // {"BASE":"http://localhost:3003/apim-server/v1/connectorProxy/v1","VERSION":"0.11.0","WITH_CREDENTIALS":true,"TOKEN":"xxx"
@@ -104,16 +110,25 @@ export class APFetch {
 
     const apsOpenAPIConfig: APSOpenAPIConfig = await APSClientOpenApi.getApsClientOpenApiConfig();
     console.log(`${logName}: apsOpenAPIConfig=${JSON.stringify(apsOpenAPIConfig, null, 2)}`);
-
+    // e.GETConnector_ZipContents(): apsOpenAPIConfig={
+    //   "BASE": "/apim-server/v1",
+    //   "VERSION": "0.4.0",
+    //   "WITH_CREDENTIALS": true,
+    //   "CREDENTIALS": "include",
+    //   "TOKEN": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJtYXN0ZXIudXNlckBhc3luYy1hcGltLWRldmVsLmNvbSIsImlhdCI6MTY2NjE3NzE2MDQ0OSwiYWNjb3VudFR5cGUiOiJVU0VSX0FDQ09VTlQiLCJleHAiOjE2NjYxNzcxNjEzNDksImlzcyI6InFzLWFzeW5jLWFwaW0tc2VydmVyIiwic3ViIjoibWFzdGVyLnVzZXJAYXN5bmMtYXBpbS1kZXZlbC5jb20ifQ.Y_nrXJLwPHnRfUtMGEOUUcvJSMzn7XyNPKbJy9xEdRY"
+    // }
     const base = connectorOpenApiConfig.BASE + '/';
     console.log(`${logName}: connectorUrlPath=${connectorUrlPath}, base=${base}`);
+    // e.GETConnector_ZipContents(): connectorUrlPath=ep2-demo/apiProducts/hid4z4jh9db-bronze/apis/NewConsumerApi-bpgli1jzier?format=application%2Fzip, base=/apim-server/v1/connectorProxy/v1/
+
 
     // distinguish between using browser bar host/port and specific host/port
-    let url: URL;
+    let urlString: string;
     if(base.includes('http')) {
-      url = new URL(connectorUrlPath, base);
+      const url = new URL(connectorUrlPath, base);
+      urlString = url.toString();
     } else {
-      url = new URL(base + connectorUrlPath);
+      urlString = base + connectorUrlPath;
     }
 
     const headers = new Headers({
@@ -126,7 +141,7 @@ export class APFetch {
       credentials: 'include',
       headers: headers,
     };
-    const response = await window.fetch( url.toString(), requestInit);
+    const response = await window.fetch( urlString, requestInit);
     if(!response.ok) {
       throw new ApiError(response, logName);
     }
